43b8aa6c07c0e8448b31ded0452a4c1d4a5732f1
mrather2011 <maxprop26@gmail.com>
2020-08-30 21:54:46 +0300

    progress but changes needed

--- src/components/navigation/navbar/Navbar.js
+++ src/components/navigation/navbar/Navbar.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import SideDrawer from "../sidedrawer/SideDrawer";
 import { FaAlignJustify } from "react-icons/fa";
 import classes from "./Navbar.module.scss";
 
 const Navbar = (props) => {
+  const [sideDrawerOpen, toggleSideDrawer] = useState(false);
+
+  const sideDrawerToggle = () => {
+    toggleSideDrawer(!sideDrawerOpen);
+    console.log(sideDrawerOpen);
+  };
+
   return (
-    <div className={classes.Container}>
-      <div className={classes.LogoContainer}>
-        <h1>LOGO</h1>
-      </div>
-      <div className={classes.DropdownContainer}>
-        <FaAlignJustify size="2.5rem" />
+    <div
+      onClick={sideDrawerToggle}
+      className={[
+        classes.Backdrop,
+        sideDrawerOpen ? classes.BackdropOpen : classes.BackdropClosed,
+      ].join(" ")}
+    >
+      <SideDrawer toggleSideDrawer={toggleSideDrawer} sDOpen={sideDrawerOpen} />
+      <div className={classes.Container}>
+        <div className={classes.LogoContainer}>
+          <h1>LOGO</h1>
+        </div>
+        <div onClick={sideDrawerToggle} className={classes.DropdownContainer}>
+          <FaAlignJustify size="2.5rem" />
+        </div>
       </div>
     </div>
   );
--- src/components/navigation/sidedrawer/SideDrawer.js
+++ src/components/navigation/sidedrawer/SideDrawer.js
@@ -1,35 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./sidedrawer.module.scss";
 import { Link } from "react-router-dom";
 
-const Navbar = (props) => {
+const SideDrawer = ({ sDOpen }) => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link>About</Link>
-        </li>
-        <li>
-          <Link>Approach</Link>
-        </li>
-        <li>
-          <Link>Case Studies</Link>
-        </li>
-        <li>
-          <Link>Active Listings</Link>
-        </li>
-        <li>
-          <Link>Valuation</Link>
-        </li>
-        <li>
-          <Link>Testimonials</Link>
-        </li>
-        <li>
-          <Link>Contact</Link>
-        </li>
-      </ul>
+    <div
+      className={[
+        classes.Container,
+        sDOpen ? classes.ContainerOpen : classes.ContainerClosed,
+      ].join(" ")}
+    >
+      <div
+        className={[
+          classes.Backdrop,
+          sDOpen ? classes.BackdropOpen : classes.BackdropClosed,
+        ].join(" ")}
+      >
+        <div
+          className={[
+            classes.NavItems,
+            sDOpen ? classes.NavItemsOpen : classes.NavItemsClosed,
+          ].join(" ")}
+        >
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/approach">Approach</Link>
+            </li>
+            <li>
+              <Link to="/case-studies">Case Studies</Link>
+            </li>
+            <li>
+              <Link to="active-listings">Active Listings</Link>
+            </li>
+            <li>
+              <Link>Valuation</Link>
+            </li>
+            <li>
+              <Link>Testimonials</Link>
+            </li>
+            <li>
+              <Link>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Navbar;
+export default SideDrawer;
